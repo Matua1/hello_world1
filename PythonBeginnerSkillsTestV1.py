5ab852303379f039c2d526b0c70003fc555bbba0
Jim Halafihi <jihalafihi@student.phs.school.nz>
2023-05-10 00:21:50 +1200

    upload a file to repo

---
+++ PythonBeginnerSkillsTestV1.py
@@ -0,0 +1,68 @@
+# Test name: Python Skills Test for Beginners
+
+# Test instructions:
+# This test consists of 5 questions. 
+# For each question, you will be given a task to perform using Python code. 
+# You should write a function that completes the task as described, and then return the result. 
+# You should not use any external libraries or modules to complete the tasks. 
+# You should not define any global variables. 
+# You may define additional helper functions if necessary.
+
+# Test questions:
+
+# Question 1:
+# Write a function that takes an integer as an argument and returns the absolute value of the integer.
+# Example: 
+# input: -5
+# output: 5
+def absolute_value(n):
+    # your code here
+
+# Question 2:
+# Write a function that takes a string as an argument and returns the string with the first and last characters removed.
+# Example:
+# input: "hello"
+# output: "ell"
+def remove_first_last(string):
+    # your code here
+
+# Question 3:
+# Write a function that takes a list of integers as an argument and returns the sum of all odd numbers in the list.
+# Example:
+# input: [1, 2, 3, 4]
+# output: 4
+def sum_odds(numbers):
+    # your code here
+
+# Question 4:
+# Write a function that takes a list of strings as an argument and returns a new list with all strings that have length 4 or less.
+# Example:
+# input: ["cat", "dog", "bird", "elephant"]
+# output: ["cat", "dog"]
+def filter_short_strings(strings):
+    # your code here
+
+# Question 5:
+# Write a function that takes a dictionary as an argument and returns a new dictionary with all key-value pairs where the key is a string.
+# Example:
+# input: { "a": 1, "b": 2, "c": 3, 5: "d" }
+# output: { "a": 1, "b": 2, "c": 3 }
+def filter_string_keys(dictionary):
+    # your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
